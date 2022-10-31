f27317983ffa558bc0859e822c1922c0f8d40522
jesus-ccapa <jesus.ccapa@tecsup.edu.pe>
2022-10-31 16:06:14 -0500

    integracion de todo el sistema de snapchat

---
+++ CcapaSnapchat/iniciarSesionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  CcapaSnapchat
+//
+//  Created by Mac 11 on 31/10/22.
+//
+
+import UIKit
+import FirebaseAuth
+
+
+class iniciarSesionViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in print ("Intentando Iniciar Sesion")
+            if error != nil{
+                print("Se presento el siguiente error: \(error)")
+            }else{
+                print("Inicio de sesion exitoso")
+            }
+        }
+    }
+    
+}
+
